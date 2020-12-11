15460384ee7fb5860e8c7abd097d33a861bc2856
ALDeig <59621369+ALDeig@users.noreply.github.com>
2020-12-11 17:34:16 +0400

    Создание дотэнв и изменения в конфиге

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 KEY_EGR = os.getenv('KEY_EGR')
 KEY_BANK_INFO = os.getenv('KEY_BANK_INFO')
