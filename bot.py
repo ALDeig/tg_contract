3457ff7a7f659695a7da97fa1bf4756fe50711bd
aldi <alkin.denis@gmail.com>
2021-04-06 21:22:33 +0400

    Много изменений

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ async def set_default_commands(dp):
         # types.BotCommand("get_reviews", "Отзывы (только для администратора)"),
         # types.BotCommand("send_message", "Отправить сообщение всем пользователям (только для администратора)"),
         # types.BotCommand("get_limit", "Остаток ИНН и БИК (только для администартора)"),
-        # types.BotCommand("save_cameras", "Сохранить данные из таблицы в базу (только для админа)")
+        # types.BotCommand("save_data", "Сохранить данные из таблицы в базу (только для админа)")
     ])
 
 
